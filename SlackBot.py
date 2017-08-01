1f4eff1146efde01d7fddb36d3c41ad3d827b966
Daniel Henry <email378637@gmail.com>
2017-08-01 11:52:23 -0700

    Update SlackBot.py

--- SlackBot.py
+++ SlackBot.py
@@ -1,8 +1,6 @@
 from slackclient import SlackClient
 
-
-slack_token = "xoxp-5158427403-89070235363-221333790775-494335af088df84f1524c8b60360baf8"
-#slack_token = 'xoxb-133288543732-fcfZ7i3KH9HtTTyb78ZAj0EG'
+slack_token = ""
 sc = SlackClient(slack_token)
 timeStamp = []
 while True:
