e0c1f403367e99007a01a8f2da911079e9429f06
Daniel Henry <email378637@gmail.com>
2017-08-15 15:12:56 -0700

    Update SlackPortal.py

--- SlackPortal.py
+++ SlackPortal.py
@@ -1,36 +1,65 @@
 from slackclient import SlackClient
+import pickle
 
-slack_token = ""
+def load_obj(name):
+    try:
+        with open(name + '.pkl', 'rb') as f:
+            return pickle.load(f)
+    except:
+        return []
+    
+def save_obj(obj, name):
+    with open(name + '.pkl', 'wb') as f:
+        pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
+
+slack_token = "SLACK TOKEN HERE"
 sc = SlackClient(slack_token)
-timeStamp = []
+
+timeStamp = load_obj('timeStamp')
+
+chatName = input('Enter chat name (general, dmChat): ')
+channels = {'general' : 'C054NCKDB', 'dmChat' : 'G6N5DJ7J7'}
 while True:
     chat = input('Enter chat: ')
     if chat == 'deleteAll':
         for time in timeStamp:
             sc.api_call(
                 "chat.delete",
-                channel="C054NCKDB",
+                channel=channels[chatName],
                 ts=time
                 )
-    elif chat == 'delete':
-        sc.api_call(
-            "chat.delete",
-            channel="C054NCKDB",
-            ts=timeStamp[len(timeStamp) - 1]
-            )
             
+    elif chat == 'delete':
+        if timeStamp:
+            sc.api_call(
+                    "chat.delete",
+                    channel=channels[chatName],
+                    ts=timeStamp[len(timeStamp) - 1]
+                    )
+            del timeStamp[len(timeStamp) - 1]
+        else:
+            print('all messages deleted')
+            
+    elif chat == 'deleteLast':
+        num = int(input('delete back how many posts?: '))
+        for x in range(num):
+            if timeStamp:
+                sc.api_call(
+                        "chat.delete",
+                        channel=channels[chatName],
+                        ts=timeStamp[len(timeStamp) - 1]
+                        )
+                del timeStamp[len(timeStamp) - 1]
+            else:
+                print('all messages deleted')
     else:
         info = sc.api_call(
             "chat.postMessage",
-            channel="#general_go2",
+            channel=channels[chatName],
             text=chat,
             as_user = 'false',
             username='whyy',
             icon_url='https://ca.slack-edge.com/T054NCKBV-U2M226XAP-9db940ec6597-48'
             )
         timeStamp.append(info['ts'])
-'''
-groups = sc.api_call("groups.list")
-for item in groups['groups']:
-    print(item['id'], item['name'])
-'''
+    save_obj(timeStamp, 'timeStamp')
